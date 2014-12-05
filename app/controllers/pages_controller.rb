628051ad8667e3e55f66313d124e4e76b742e401
abouvier <abouvier@currentmarketing.com>
2014-12-05 10:18:11 -0500

    added layouts, css, and js from previous version

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
     def index
     end
+
+    def work
+    end
+
+    def culture
+    end
+
+    def contact
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  match '/contact' => 'pages#contact', via: [:get]
+  match '/culture' => 'pages#culture', via: [:get]
+  match '/work' => 'pages#work', via: [:get]
   root 'pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
