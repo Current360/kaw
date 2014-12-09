10ffeac826bab1f9df719f0bf3f3768dcf65f843
abouvier <abouvier@currentmarketing.com>
2014-12-09 16:38:09 -0500

    added title and description functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # Prints a default title if we haven't specifically overridden it
+  def full_title(page_title)
+    base_title = "Integrated Marketing, Branding, Social, Digital | Louisville, KY"
+    if page_title.empty?
+      base_title
+    else
+      page_title
+    end
+  end
 end
